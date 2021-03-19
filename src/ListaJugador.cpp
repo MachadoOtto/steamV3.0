e039ffe5a62a964e5ebc67def8d0a7e08c8259c6
Alexis Baladon Ferreira De Araujo <alexis.baladon@fing.edu.uy>
2021-03-19 08:32:55 -0300

    Replace ListaJugador.cpp

--- src/ListaJugador.cpp
+++ src/ListaJugador.cpp
@@ -1,7 +1,9 @@
-#include "ListaJugador.h"
+#include "../include/ListaJugador.h"
 
 ListaJugador::ListaJugador(Jugador nuevoJugador) {
     jugador = nuevoJugador;
+    jugador->siguiente = NULL;
+    jugador->anterior = NULL;
 }
 
 void ListaJugador::setJugador(Jugador nuevoJugador) {
