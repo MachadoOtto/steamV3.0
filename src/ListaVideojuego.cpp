170848124f4f86c46731b84acac36714310dfa16
Alexis Baladon Ferreira De Araujo <alexis.baladon@fing.edu.uy>
2021-03-19 08:33:06 -0300

    Replace ListaVideojuego.cpp

--- src/ListaVideojuego.cpp
+++ src/ListaVideojuego.cpp
@@ -1,7 +1,9 @@
-#include "ListaVideojuego.h"
+#include "../include/ListaVideojuego.h"
 
 ListaVideojuego::ListaVideojuego(Videojuego nuevoVideojuego) {
     videojuego = nuevoVideojuego;
+    videojuego->siguiente = NULL;
+    videojuego->anterior = NULL;
 }
 
 void ListaJugador::setVideojuego(Videojuego nuevoVideojuego) {
