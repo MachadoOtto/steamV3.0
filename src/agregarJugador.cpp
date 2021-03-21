259261c042482cc00de130f93325e51ed0bfe057
Alexis Baladon Ferreira De Araujo <alexis.baladon@fing.edu.uy>
2021-03-21 20:55:56 -0300

    mi funcion (falta agregegar el jugador a la lista)

---
+++ src/agregarJugador.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void Sistema::agregarJugador(std::string nickname, int edad, std::string contraseña) {
+    if(estaJugadorSistema(nickname)) {
+        throw std::invalid_argument("Ya hay un jugador con ese nombre");
+    }
+    else {
+        jugadores.(nickname,edad,contraseña); //funcion que agrega jugador a lista
+    }
+}
