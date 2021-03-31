132ee69ace2263718be492a30c69b51716fa65b4
Gtoyos <guille.toyos@gmail.com>
2021-03-31 01:05:27 -0300

    final2

--- Ejercicio2/Ejercicio2SinFD/main.cpp
+++ Ejercicio2/Ejercicio2SinFD/main.cpp
@@ -1,44 +1,14 @@
 #include <iostream>
-
-class A {
-    private:
-        int a = 1;
-        B * b;
-        C * c;
-    public:
-        void output() {
-            std::cout << a << ") La clase A funciona" << std::endl;
-        }
-};
-
-class B {
-    private:
-        A * a;
-        int b = 2;
-        C * c;
-    public:
-        void output() {
-            std::cout << b << ") La clase B funciona" << std::endl;
-        };
-};
-
-class C {
-    private:
-        A * a;
-        B * b;
-        int c = 3;
-    public:
-        void output() {
-            std::cout << c << ") La clase C funciona" << std::endl;
-        };
-};
+#include "include/A.h"
+#include "include/B.h"
+#include "include/C.h"
 
 int main() {
-    A claseA;
-    B claseB;
-    C claseC;
-    claseA.output();
-    claseB.output();
-    claseC.output();
+    claseA a;
+    claseB b;
+    claseC c;
+    a.output();
+    b.output();
+    c.output();
     return 0;
 }
