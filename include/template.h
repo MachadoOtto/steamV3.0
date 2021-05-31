70dfd7c3db9e5f7623a97229dac6a576ec26119f
Gtoyos <guillermo.toyos@fing.edu.uy>
2021-05-31 19:34:46 -0300

    File templates added.

---
+++ include/template.h
@@ -0,0 +1,10 @@
+/* Laboratorio Programacion IV - INCO/FING/UDELAR
+ * Laboratorio 4 - Class declaration
+ * Autores (por nombre):
+ * 	Alexis Baladon
+ * 	Guillermo Toyos
+ * 	Jorge Machado
+ * 	Juan Jose Mangado
+ * 	Mathias Ramilo
+ */
+
