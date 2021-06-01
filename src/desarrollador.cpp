d48716c1c9cd23ee689f1d7a7952b147ab98b99a
Alexis Baladon Ferreira De Araujo <alexis.baladon@fing.edu.uy>
2021-06-01 13:40:29 -0300

    Upload New File

---
+++ src/desarrollador.cpp
@@ -0,0 +1,15 @@
+#include "../include/desarrollador.h"
+#include "../include/estadistica.h"
+
+/*Set(String) Desarrollador::getVideojuegosDesarrollados {
+
+}*/
+//void publishVideogame(DtVideojuego,Set(Categoria));
+
+void Desarrollador::suscribirEstadistica(Estadistica) {
+
+}
+
+/*Set(DtEstadistica) Desarrollador::solicitarEstadisticas(Videjuego) {
+
+}*/
