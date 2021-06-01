3e31c0753f10c2506eb9be8e195b6e32c4b9c3c1
Mathias Nicolas Ramilo Villar <mathias.ramilo@fing.edu.uy>
2021-06-01 14:50:58 -0300

    Update Videojuego.h

--- include/Videojuego.h
+++ include/Videojuego.h
@@ -1,3 +1,13 @@
+/* Laboratorio Programacion IV - INCO/FING/UDELAR
+ * Laboratorio 4 - Class declaration
+ * Autores (por nombre):
+ * 	Alexis Baladon
+ * 	Guillermo Toyos
+ * 	Jorge Machado
+ * 	Juan Jose Mangado
+ * 	Mathias Ramilo
+ */
+
 #ifndef VIDEOJUEGO
 #define VIDEOJUEGO
 
