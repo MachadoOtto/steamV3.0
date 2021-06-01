086cea8bd52415127bf65ad92fc31e46d822c3f9
Alexis Baladon Ferreira De Araujo <alexis.baladon@fing.edu.uy>
2021-06-01 13:18:41 -0300

    Upload New File

---
+++ include/dtPartida.h
@@ -0,0 +1,16 @@
+#ifndef DTPARTIDA
+#define DTPARTIDA
+
+#include "dtFechaHora.h"
+
+class DtPartida {
+    private:
+        int id;
+        DtFechaHora fecha;
+        float duracion;
+        bool activa;
+    public:
+
+};
+
+#endif
