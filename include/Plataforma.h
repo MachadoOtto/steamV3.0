97e3c5aa4ab37f3554272421d7c00a358186e91c
Mathias Nicolas Ramilo Villar <mathias.ramilo@fing.edu.uy>
2021-06-01 19:07:38 -0300

    Add new file

---
+++ include/Plataforma.h
@@ -0,0 +1,23 @@
+/* Laboratorio Programacion IV - INCO/FING/UDELAR
+ * Laboratorio 4 - Class declaration
+ * Autores (por nombre):
+ * 	Alexis Baladon
+ * 	Guillermo Toyos
+ * 	Jorge Machado
+ * 	Juan Jose Mangado
+ * 	Mathias Ramilo
+ */
+
+#ifndef PLATAFORMA
+#define PLATAFORMA
+
+#include <string>
+
+class Plataforma::Categoria {
+    private:
+    
+    public:
+
+}
+
+#endif
