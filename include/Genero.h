b5b9bff5f0f6a1ca7ac8bd57fc4d9637277566dc
Mathias Nicolas Ramilo Villar <mathias.ramilo@fing.edu.uy>
2021-06-01 18:29:15 -0300

    Add new file

---
+++ include/Genero.h
@@ -0,0 +1,23 @@
+/* Laboratorio Programacion IV - INCO/FING/UDELAR
+ * Laboratorio 4 - Class declaration
+ * Autores (por nombre):
+ * 	Alexis Baladon
+ * 	Guillermo Toyos
+ * 	Jorge Machado
+ * 	Juan Jose Mangado
+ * 	Mathias Ramilo
+ */
+
+#ifndef GENERO
+#define GENERO
+
+#include <string>
+
+class Genero::Categoria {
+    private:
+
+    public:
+        
+}
+
+#endif
