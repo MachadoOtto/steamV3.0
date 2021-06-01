eb4541dc9ad7689825b361322acead034dcba407
Alexis Baladon Ferreira De Araujo <alexis.baladon@fing.edu.uy>
2021-06-01 13:19:11 -0300

    Upload New File

---
+++ include/dtPartidaMultijugador.h
@@ -0,0 +1,13 @@
+#ifndef DTPARTIDAMULTIJUGADOR
+#define DTPARTIDAMULTIJUGADOR
+
+#include "dtPartida.h"
+
+class DtPartidaMultijugador: public DtPartida {
+    private:
+        bool transmitidaEnVivo;
+    public:
+
+};
+
+#endif
