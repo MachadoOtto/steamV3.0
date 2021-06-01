fc907064fc0c6e72bf45274c276679cbc60030d4
Mathias Nicolas Ramilo Villar <mathias.ramilo@fing.edu.uy>
2021-06-01 14:50:30 -0300

    Add new file Videojuego.h

---
+++ include/Videojuego.h
@@ -0,0 +1,47 @@
+#ifndef VIDEOJUEGO
+#define VIDEOJUEGO
+
+class Videojuego {
+    private:
+        std::string nombre;
+        std::string descripcion;
+        DtPrecios costoSuscripciones;
+        float totalHorasJugadas;
+        float puntaje;
+        int totalJugadoresSuscriptos;
+
+        //Associations
+        Suscripcion* suscripciones;
+        map<int,Partida> *partidas;
+        Categoria* categorias;
+        
+    public:
+        Videojuego(DtVideojuego,Set(Categoria));
+        DtVideojuego obtenerDatoVideojuego();
+        Set(string) obtenerJugadoresSubscriptos();
+        bool estaActivo();
+        void confirmarPartida(Jugador,bool,bool,Set(Jugador));
+        void cancelarSuscripcion(Jugador);
+        void confirmarSuscripcion(Jugador,TipoSuscripcion,TipoPago);
+        void eliminarInfoAsociada(Videojuego);
+
+        //Getters
+        std::string getNombre();
+        std::string getDescripcion();
+        DtPrecios getCostoSuscripciones();
+        float getTotalHorasJugadas();
+        float getPuntaje();
+        int getTotalJugadoresSuscriptos();
+        Suscripcion* getSuscripciones();
+        map<int,Partida> getPartidas();
+        Categoria* getCategorias();
+
+        //Setters
+        void setNombre(std::string);
+        void setDescripcion(std::string);
+        void setCostoSuscripciones(DtPrecios);
+        void setTotalHorasJugadas(float);
+        void setPuntaje(float);
+        void setTotalJugadoresSuscriptos(int);
+}
+#endif
