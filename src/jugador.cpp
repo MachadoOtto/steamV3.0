d3ca5f0df28873e188fe7d0d3e8e02757a89c204
Alexis Baladon Ferreira De Araujo <alexis.baladon@fing.edu.uy>
2021-06-01 13:41:38 -0300

    Upload New File

---
+++ src/jugador.cpp
@@ -0,0 +1,47 @@
+#include "../include/jugador.h"
+#include "../include/dtVideojuego.h"
+#include "../include/dtPartida.h"
+#include "../include/dtPartidaindividual.h"
+#include "../include/partidaindividual.h"
+#include "../include/partida.h"
+#include "../include/suscripcion.h"
+
+/*Set(String) obtenerVidejuegosActivos() {
+
+}*/
+
+/*Set(DtVidejuego) obtenerDatosVj() {
+
+}*/
+
+/*Set(String) getVjSinPartidasActivas() {
+
+}*/
+
+/*Set(DtPartida) obtenerPartidasActivas() {
+
+}*/
+
+/*Set(DtPartidaindividual) obtenerHistorialPartidas() {
+
+}*/
+
+Partidaindividual seleccionarContinuacionPartida(int) {
+
+}
+
+void agregarPartida(Partida) {
+
+}
+
+void agregarSuscripcion(Suscripcion) {
+
+}
+
+void abandonarPartidaMulti(PartidaMultijugador) {
+
+}
+
+void finPartida(int) {
+
+}
