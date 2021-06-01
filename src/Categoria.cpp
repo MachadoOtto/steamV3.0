1fd7200194bb93302cff724d16f02010d145386d
Mathias Nicolas Ramilo Villar <mathias.ramilo@fing.edu.uy>
2021-06-01 19:28:39 -0300

    Add new file

---
+++ src/Categoria.cpp
@@ -0,0 +1,15 @@
+/* Laboratorio Programacion IV - INCO/FING/UDELAR
+ * Laboratorio 4 - Class declaration
+ * Autores (por nombre):
+ * 	Alexis Baladon
+ * 	Guillermo Toyos
+ * 	Jorge Machado
+ * 	Juan Jose Mangado
+ * 	Mathias Ramilo
+ */
+
+#include "Categoria.h"
+
+Categoria::Categoria() {
+    
+}
