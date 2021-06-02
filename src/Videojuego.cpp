fcaa2709e99cec55b9fe51ea5955ff4ec343e555
Mathias Nicolas Ramilo Villar <mathias.ramilo@fing.edu.uy>
2021-06-02 19:26:01 -0300

    Update src/Videojuego.cpp

--- src/Videojuego.cpp
+++ src/Videojuego.cpp
@@ -11,28 +11,54 @@
 #include <string>
 
 #include "Videojuego.h"
+#include "Partida.h"
+#include "PartidaIndividual.h"
+#include "PartidaMultijugador.h"
 
 Videojuego::Videojuego(DtVideojuego datos, Set(Categoria) categorias) {
-    this.nombre = datos.getNombre();
-    this.descripcion = datos.getDescripcion();
-    this.costoSuscripciones = datos.getCostos();
-    this.totalHorasJugadas = 0;
-    this.puntaje = 0;
-    this.totalJugadoresSuscriptos = 0;
+    this->nombre = datos.getNombre();
+    this->descripcion = datos.getDescripcion();
+    this->costoSuscripciones = datos.getCostos();
+    this->totalHorasJugadas = 0;
+    this->puntaje = 0;
+    this->totalJugadoresSuscriptos = 0;
 
     ptrCat = begin(categorias);
     while (ptrCat != NULL) {
-        this.categorias = add(categorias.getDt());
+        this->categorias = add(categorias->getDt());
         ptrCat = next(categorias);
     }
 }
 
 DtVideojuego Videojuego::obtenerDatosVideojuego() {
     DtVideojuego dtVid = new DtVideojuego();
-    dtVid.nombre = this.getNombre();
-    dtVid.descripcion = this.getDescripcion();
-    dtVid.costos = this.getCostoSuscripciones();
+    dtVid->nombre = this->getNombre();
+    dtVid->descripcion = this->getDescripcion();
+    dtVid->costos = this->getCostoSuscripciones();
     return dtVid;
 }
 
-Set(string) Videojuego::
+Set(string) Videojuego::obtenerJugadoresSuscriptos() {
+
+}
+
+bool Videojuego::estaActivo() {
+    if (this->partidas->empty()) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+void Videojuego::confirmarPartida(Jugador host,PartidaIndividual pCont,bool enVivo,Set(Jugador) jUnidos) {
+    if (pCont != NULL) {
+        PartidaIndividual pInd = new PartidaIndividual();
+        pInd->id = obtenerDatosPartida()->getId();
+        pInd->fecha = obtenerDatosPartida()->getFecha();
+        pInd->duracion = obtenerDatosPartida()->getDuracion();
+        pInd->activa = obtenerDatosPartida()->getActiva();
+        host->partidas = add(pInd); 
+    } else {
+
+    }
+}
