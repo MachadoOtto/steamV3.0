1029bec41d77576de8119454bf26bd8f00fc7f75
Alexis Baladon Ferreira De Araujo <alexis.baladon@fing.edu.uy>
2021-06-03 10:19:13 -0300

    Update desarrollador.h

--- include/desarrollador.h
+++ include/desarrollador.h
@@ -8,13 +8,16 @@
 
 #include <string>
 #include <list>
+#include <map>
 
 class Desarrollador: public Usuario {
     private:
         std::string empresa;
+        std::map<std::string,Videojuego> * videojuegosDesarrollados;
+        std::map<std::string,Estadistica> * estadisticas;
     public:
-        std::list<std::string> getVideojuegosDesarrollados;
-        void publishVideogame(DtVideojuego,std::list<Categoria>);
+        std::map<std::string,Videojuego> * getVideojuegosDesarrollados();
+        void publishVideogame(* DtVideojuego,* std::map<std::string,Categoria>);
         void suscribirEstadistica(Estadistica);
         std::list<DtEstadistica> solicitarEstadisticas(Videjuego);
 };
