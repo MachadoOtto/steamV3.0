b3058c00a51ebb8ea425e4ab850e2ab2fd54de9c
Alexis Baladon Ferreira De Araujo <alexis.baladon@fing.edu.uy>
2021-06-03 16:56:09 -0300

    Update include/dtPartidaMultijugador.h, src/dtPartidaMultijugador.cpp files

--- include/dtPartidaMultijugador.h
+++ include/dtPartidaMultijugador.h
@@ -2,12 +2,13 @@
 #define DTPARTIDAMULTIJUGADOR
 
 #include "dtPartida.h"
+#include "dtFechaHora"
 
 class DtPartidaMultijugador: public DtPartida {
     private:
         bool transmitidaEnVivo;
     public:
-
+        DtPartidaMultijugador(int,DtFechaHora,float,bool,bool transmitidaEnVivo);
 };
 
 #endif
--- src/dtPartidaMultijugador.cpp
+++ src/dtPartidaMultijugador.cpp
@@ -1,1 +1,7 @@
 #include "../include/dtPartidaMultijugador.h"
+#include "../include/dtPartida.h"
+#include "../include/dtFechaHora"
+
+DtPartidaMultijugador::DtPartidaMultijugador(int id,DtFechaHora fecha,float duracion,bool activa, bool transmitidaEnVivo): DtPartida(id,fecha,duracion,activa) {
+    this->transmitidaEnVivo = transmitidaEnVivo;
+}
