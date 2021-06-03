520735c98c2414b8f05117b65789a10b416fa2d2
Alexis Baladon Ferreira De Araujo <alexis.baladon@fing.edu.uy>
2021-06-03 11:36:10 -0300

    Update desarrollador.cpp

--- src/desarrollador.cpp
+++ src/desarrollador.cpp
@@ -4,17 +4,19 @@
 
 #include <string>
 #include <list>
+#include <map>
 
-std::list<std::string> Desarrollador::getVideojuegosDesarrollados {
-
+std::list<std::string> * Desarrollador::getVideojuegosDesarrollados() {
+    return getVideojuegosDesarrollados;
 }
 
-void Desarrollador::publishVideogame(DtVideojuego,std::list<Categoria>) {
+void Desarrollador::publishVideogame(DtVideojuego * dtvj,std::map<std::string,Categoria> * categorias) {
+    Videjuego * vj = create(dtvj,categorias); //el new?
+    videojuegosDesarrollados->insert(vj);
+}
+
+void Desarrollador::suscribirEstadistica(Estadistica est) {
     
-}
-
-void Desarrollador::suscribirEstadistica(Estadistica) {
-
 }
 
 std::list<DtEstadistica> Desarrollador::solicitarEstadisticas(Videjuego) {
