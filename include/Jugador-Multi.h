0e3b17c1cf5f9b9c128fcf054c6058656341d0c4
Mathias Nicolas Ramilo Villar <mathias.ramilo@fing.edu.uy>
2021-06-03 12:11:05 -0300

    Update Jugador-Multi.h

--- include/Jugador-Multi.h
+++ include/Jugador-Multi.h
@@ -1,3 +1,15 @@
+/* Laboratorio Programacion IV - INCO/FING/UDELAR
+ * Laboratorio 4 - Class declaration
+ * Autores (por nombre):
+ * 	Alexis Baladon
+ * 	Guillermo Toyos
+ * 	Jorge Machado
+ * 	Juan Jose Mangado
+ * 	Mathias Ramilo
+ */
+
+#include "DtFechaHora.h"
+
 class Jugador-Multi {
     private:
         DtFechaHora desconexion;
