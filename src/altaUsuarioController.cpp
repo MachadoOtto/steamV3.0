999e25a9895aaa78ca9c9b6cfebd658c5dab2b49
Juan JosÃ© Mangado Esteves <juan.mangado@fing.edu.uy>
2021-06-05 15:43:51 -0300

    Upload New File

---
+++ src/altaUsuarioController.cpp
@@ -0,0 +1,59 @@
+/* Laboratorio Programacion IV - INCO/FING/UDELAR
+ * Laboratorio 4
+ * Autores (por nombre):
+ * 	Alexis Baladon 
+ * 	Guillermo Toyos
+ * 	Jorge Machado
+ * 	Juan Jose Mangado
+ * 	Mathias Ramilo
+ */
+
+#include "../include/altaUsuarioController.h"
+
+altaUsuarioController::altaUsuarioController(){
+    datos = NULL;
+}
+
+altaUsuarioController * altaUsuarioController::getInstance(){
+   if(instancia == NULL)
+      instancia = new altaUsuarioController;
+   return instancia;
+}
+
+void ingresarUsuario(dtUsuario * d){
+   datos = d;   
+}
+
+void altaUsuarioController::ingresarEmpresa(string e){
+   empresa = e;
+}
+
+void altaUsuarioController::ingresarNickname(string n){
+   handlerUsuario * hc = handlerUsuario::getInstance();
+   if (!hc->existeJugador(n))
+      nickname = n;
+}
+
+void altaUsuarioController::ingresarDescripcion(string d){
+   descripcion = d;
+}
+
+void altaUsuarioController::clearCache(){
+   delete datos;
+}
+
+void altaUsuarioController::confirmarDarDeAltaUsuario(){
+     
+   HandlerUsuario * hu = HandlerUsuario::getInstance();
+   if(empresa != NULL)
+      desarrollador * usu(email, contraseña, empresa);
+   else
+      jugador * usu(email, contraseña, nickname, descripcion);
+   hu->addUsuario(usu); 
+}
+
+altaUsuarioController::~altaUsuarioController(){
+   this->clearCache();
+   instancia = NULL; 
+}
+
