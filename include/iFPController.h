148e411caa0e72b2f437c9b422845b6e53f2b1c5
Alexis Baladon Ferreira De Araujo <alexis.baladon@fing.edu.uy>
2021-06-05 16:01:36 -0300

    Update include/desarrollador.h, include/dtPartida.h, include/dtPartidaIndividual.h, include/dtPartidaMultijugador.h, include/iFPController.h, include/iIFPController.h, include/jugador.h, include/usuario.h, src/desarrollador.cpp, src/dtPartida.cpp, src/dtPartidaIndividual.cpp, src/dtPartidaMultijugador.cpp, src/iFPController.cpp, src/jugador.cpp, src/usuario.cpp, main.cpp files

--- include/iFPController.h
+++ include/iFPController.h
@@ -1,3 +1,12 @@
+/* Laboratorio Programacion IV - INCO/FING/UDELAR
+ * Integrantes:
+ *      Alexis Baladon (5.574.612-4) - alexis.baladon@fing.edu.uy
+ *      Guillermo Toyos (5.139.879-9) - guillermo.toyos@fing.edu.uy
+ *      Jorge Machado (4.876.616-9) - jorge.machado.ottonelli@fing.edu.uy
+ *      Juan Jose Mangado (5.535.227-0) - juan.mangado@fing.edu.uy
+ *      Mathias Ramilo (5.665.788-5) - mathias.ramilo@fing.edu.uy
+ */
+
 #ifndef IFPCONTROLLER
 #define IFPCONTROLLER
 
