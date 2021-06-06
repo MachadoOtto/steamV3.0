926b03ec8b9c0a56a1b862cec975ffdfedec45b2
Gtoyos <guillermo.toyos@fing.edu.uy>
2021-06-06 17:18:23 -0300

    implementacion menu y sistema mas resolucion del problema de la fecha del sistema.

---
+++ src/sistema.cpp
@@ -0,0 +1,22 @@
+/* Laboratorio Programacion IV - INCO/FING/UDELAR
+ * Laboratorio 4 - Class implementation 
+ * Autores (por nombre):
+ * 	Alexis Baladon
+ * 	Guillermo Toyos
+ * 	Jorge Machado
+ * 	Juan Jose Mangado
+ * 	Mathias Ramilo
+ */
+
+
+#include "../include/sistema.h"
+
+Sistema::Sistema(){
+//Hago las cosas pertinentes para iniciar el sistema...
+}
+
+Sistema::~Sistema(){
+//Hago las cosas pertinentes para cerrar el sistema... (se libera memoria)
+}
+
+
