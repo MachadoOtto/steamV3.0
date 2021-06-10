68432c187a46b8e74358de2364ed6195bbe0917a
Alexis Baladon Ferreira De Araujo <alexis.baladon@fing.edu.uy>
2021-06-10 07:45:01 -0300

    Update iFPController.h

--- include/iFPController.h
+++ include/iFPController.h
@@ -56,6 +56,12 @@ class IFPController: public IIFPController {
         void confirmarFinalizarPartida(int);
         void clearCache();
         ~IFPController();
+                
+        //abandonarPartidaMulti.cpp
+
+        std::set<DtPartidaMultijugador> obtenerPartidasMultiActivas();
+        void confirmarAbandonarPartida(int);
+
 };
 
 #endif
